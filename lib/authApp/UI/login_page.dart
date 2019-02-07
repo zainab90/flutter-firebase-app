ca77fcbca476b5dc7a126642a3202fa5831ec3c0
Zainab Salim <zainabsalim1990@gmail.com>
2019-02-07 23:35:10 +0300

    Add sign-in with Facebook

--- lib/authApp/UI/login_page.dart
+++ lib/authApp/UI/login_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+
+
+
 
 
 class LoginPage extends StatefulWidget{
@@ -17,6 +21,11 @@ class StateOfLoginPage extends State<LoginPage>{
   TextEditingController _emailController;
   TextEditingController _passwordController;
   GoogleSignIn _googleSignIn= new GoogleSignIn();
+  FacebookLogin _myFacebookLogin= new FacebookLogin();
+//  TwitterLogin _myTwitterLogin = new TwitterLogin(
+//    consumerKey: 'kkOvaF1Mowy4JTvCxKTV5O1WF',
+//    consumerSecret: 'ZECGsI6UUDBEUVGkJe4S5vd0FGqGxC3wMJCgsXgPRfjSwRFnyH',
+//  );
 
 
   @override
@@ -34,7 +43,7 @@ class StateOfLoginPage extends State<LoginPage>{
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text('Login'),
-        backgroundColor: Colors.blue,
+        backgroundColor: Colors.deepOrange,
       ),
       body: Container(
         padding: EdgeInsets.all(14.0),
@@ -65,7 +74,7 @@ class StateOfLoginPage extends State<LoginPage>{
             FlatButton(
               child: Text('Login'),
               textColor: Colors.white,
-              color: Colors.deepOrange,
+              color: Colors.green,
               onPressed: (){
                 // login operation( send email and password to firebase database)
                 FirebaseAuth.instance
@@ -104,14 +113,25 @@ class StateOfLoginPage extends State<LoginPage>{
                     catchError((error)
                     {print('error in login wth google is : $error');}
                     );
-                  }).catchError((){});
+                  }).catchError((e){print (e);});
 
 
                 });
 
               },
             ),
+
             SizedBox(height: 15.0),
+
+            FlatButton(
+              child: Text('Sign In with Facebook'),
+              textColor: Colors.white,
+              color: Colors.blue,
+              onPressed: _faceBookLogin
+           ),
+
+            SizedBox(height: 15.0),
+
             Text(
                 'Don\'t have an account!',
               style: TextStyle(fontStyle: FontStyle.italic, color: Colors.green.shade400, fontWeight: FontWeight.bold, fontSize: 25.0),
@@ -140,7 +160,79 @@ class StateOfLoginPage extends State<LoginPage>{
       ),)
 
     );
+
   }
+
+
+  Future<Null> _faceBookLogin() async {
+    print('call login');
+     FacebookLoginResult result = await _myFacebookLogin.logInWithReadPermissions(['email']).
+     catchError((e){print('logInWithReadPermissions : $e');});
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final FacebookAccessToken accessToken = result.accessToken;
+        FirebaseAuth.instance.signInWithFacebook(accessToken: accessToken.token).then((FirebaseUser signedUser){
+          print('user name login using facebook is : ${signedUser.displayName}');
+          print('Logged in! Token: ${accessToken.token} User id: ${accessToken.userId} Expires: ${accessToken.expires}Permissions: ${accessToken.permissions}Declined permissions: ${accessToken.declinedPermissions}');
+          Navigator.of(context).pushReplacementNamed('/authHome');
+        }).
+        catchError((e){print('FirebaseUser error is :  $e');});
+      //
+        break;
+      case FacebookLoginStatus.cancelledByUser:
+        print('Login cancelled by the user.');
+        break;
+      case FacebookLoginStatus.error:
+        print('Something went wrong with the login process.\n'
+            'Here\'s the error Facebook gave us: ${result.errorMessage}');
+        break;
+    }
+  }
+  
+
+
+// code for sign-in using twitter
+//  Future<Null> _twitterLogin () async{
+//
+//    final TwitterLoginResult result = await _myTwitterLogin.authorize().
+//    catchError((e){
+//      print('error in authorized $e');
+//    });
+//    switch (result.status) {
+//      case TwitterLoginStatus.loggedIn:
+//        FirebaseAuth.instance.signInWithTwitter(authToken: result.session.token, authTokenSecret: result.session.secret).
+//        then((FirebaseUser signedUser){
+//          print('session  username: ${result.session.username}');
+//        print('session id is ${result.session.userId}');
+//        print('signed user name in twitter is  ${signedUser.displayName}');
+//        print('signed user id is ${signedUser.uid}');
+//        Navigator.of(context).pushReplacementNamed('/authHome');
+//
+//        })
+//            .catchError((e){
+//              print('error in signedUser $e');
+//        });
+//
+//
+//
+//
+//
+//    break;
+//      case TwitterLoginStatus.cancelledByUser:
+//        print('Login cancelled by user.');
+//        break;
+//      case TwitterLoginStatus.error:
+//        print('Login error: ${result.errorMessage}');
+//        break;
+//    }
+//
+//
+//
+//
+//
+//
+//  }
 
   @override
   void dispose() {
